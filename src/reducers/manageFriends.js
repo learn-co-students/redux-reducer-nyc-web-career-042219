9e3cd5f2c11e401eed59ac8627d727d96457d286
Yasmine <y.hartung@gmail.com>
2019-07-11 11:46:39 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
-export function manageFriends(state, action){
+export function manageFriends(prevState, action){
+    switch(action.type){
+        case "ADD_FRIEND":
+            return {...prevState, friends: [...prevState.friends, action.friend]}
+        case "REMOVE_FRIEND":
+            const newFriends = prevState.friends.filter(friend => friend.id !== action.id)
+            return{...prevState, friends: newFriends}
+        default:
+            return prevState
+            
+    }
 }
